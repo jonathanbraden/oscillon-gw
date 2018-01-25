2ca2879fa2f7cf44ac12b332289db23952dcd0c2
Jonathan Braden <jonathan.n.braden@gmail.com>
2018-01-25 16:54:31 +0000

    Improvements to form factor.  Can now easily do simple calculations with both the discretely sampled latted and continuum integral approach (with somewhat poorly approximated integrals)

--- scripts/formFactor.py
+++ scripts/formFactor.py
@@ -27,7 +27,9 @@ def F1_sample(x,L,n):
     kv = (2.*np.pi/L)*np.arange(n//2+1)
     return kv,F1(x,kv)
 
-def F3_integrals(x,n,dn):
+F3_exact = lambda dr, k : 2.*(1.+np.sin(k*dr)/(k*dr))
+
+def F3_integrals(x,n,dn,ns=101):
     """
     Return the form factor by performing the angular integrals numerically instead of
     sampling off a lattice.
@@ -37,17 +39,27 @@ def F3_integrals(x,n,dn):
       n - Number of lattice sites
       dn - Bin width (in units of lattice sites)
 
-    # TO DO: Use a quadrature integrator here instead of discrete sampling
+    Output:
+      form - Angle averaged form factor
+      dn   - Sample n-values (in units of grid spacing)
+      area - Area of unit sphere normalized to 4pi (using the approximate integration here)
+
+    # TO DO: Use a quadrature integrator here instead of discrete sampling so area is actually 1
     """
     nn = n//2+1; nv = np.arange(nn); nmax = np.int(np.sqrt(3.)*nn)
     nb = np.int(np.ceil(nmax/dn)) 
-    form = np.zeros(nb)
+    form = np.empty(nb)
+
+    # Speed at expense of memory
+    tv,pv = np.meshgrid( np.linspace(0.,np.pi,ns,endpoint=False),np.linspace(0.,2.*np.pi,ns,endpoint=False) )
+    norm = (tv[0,1]-tv[0,0])*(pv[1,0]-pv[0,0]) / (4.*np.pi)
+    k_x = np.cos(tv); k_y = np.sin(tv)*np.cos(pv); k_z = np.sin(tv)*np.sin(pv); st = np.sin(tv)
+    area = norm*np.sum(st)
     
     for i in range(nb):
         ii = i*dn
-        form[i] = 0. # np.abs(np.sum(np.exp(1j*2.*np.pi*(nx*x[:,0]+ny*x[:,1]+nz*x[:,2]))))**2 
-    return form, dn*np.arange(nb)
-
+        form[i] = np.sum(st*np.abs(np.sum(np.exp(1j*2.*np.pi*ii*(k_x[...,np.newaxis]*x[:,0]+k_y[...,np.newaxis]*x[:,1]+k_z[...,np.newaxis]*x[:,2])),axis=-1))**2 )
+    return norm*form, dn*np.arange(nb), area
 
 def F3_sample(x,n,dn):
     """
@@ -71,6 +83,27 @@ def F3_sample(x,n,dn):
         w[i] += 1.
     return form, w, dn*np.arange(nb)
 
+def F3_window(x,n,dn):
+    """
+    Return form factor sampled on lattice using a Welch window
+
+    Input:
+      x  - Positions of oscillons
+      n  - Number of lattice sites (assumed same in all directions)
+      dn - bin width (not yet implemented)
+    """
+    nn = n//2+1; nv = np.arange(nn); nmax = np.int(np.sqrt(3.)*nn)
+    nb = np.int(np.ceil(nmax/dn))
+    form = np.zeros(nb); w = np.zeros(nb)
+    wtmp = np.empty(2)
+    
+    for nx,ny,nz in product(nv,nv,nv):
+        r = np.sqrt(nx**2+ny**2+nz**2)/dn; i = np.int(np.floor(r))
+        wtmp[0] = (1.-(r-i)**2)**2; wtmp[1] = (1.-(r+1-i)**2)**2 
+        form[i:i+2] += wtmp * np.abs(np.sum(np.exp(1j*2.*np.pi*(nx*x[:,0]+ny*x[:,1]+nz*x[:,2]))))**2
+        w[i:i+2] += wtmp
+    return form,w,dn*np.arange(nb)
+        
 def plot_uniform_x(nx,nl,kv=None):
     """
     Plot form factors in 1D sampled at lattice momenta for oscillons uniformly separated from each other (and thus highly correlated).  Plots the form factors up to the specified maximum number of oscillons, normalized to n_osc
